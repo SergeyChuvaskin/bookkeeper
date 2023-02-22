95bc7f6b0126de3e1243e4692cbd90d488bf7c3e
SergeyChuvaskin <Chuvsi2000@gmail.com>
2023-02-22 19:08:50 +0300

    Write  test_sqlite_repository

--- bookkeeper/repository/sqlite_repository.py
+++ bookkeeper/repository/sqlite_repository.py
@@ -1,22 +1,23 @@
 import sqlite3
+
 from typing import TypeVar, Type
+from bookkeeper.repository.abstract_repository import AbstractRepository, T
+# T = TypeVar('T')
 
-T = TypeVar('T')
-
-class SQLiteRepository:
+class SQLiteRepository(AbstractRepository[T]):
     def __init__(self, db_file: str, cls: Type[T]):
         self.db_file = db_file
         self.table_name = cls.__name__.lower()
         self.fields = [f for f in cls.__dict__.keys() if not f.startswith('__')]
 
-        # Connect to the database and create the table if it doesn't exist
+        # Подключение к базе данных и создание таблицу, если она не существует.
         self.conn = sqlite3.connect(self.db_file)
         self.cursor = self.conn.cursor()
         self.cursor.execute(f"CREATE TABLE IF NOT EXISTS {self.table_name} ({' '.join([f'{field} TEXT' for field in self.fields])}, pk INTEGER PRIMARY KEY)")
         self.conn.commit()
 
     def add(self, obj: T) -> T:
-        # Insert the object into the table
+        # добавление объекта в таблицу
         fields_str = ', '.join(self.fields)
         placeholders_str = ', '.join(['?' for _ in self.fields])
         self.cursor.execute(f"INSERT INTO {self.table_name} ({fields_str}, pk) VALUES ({placeholders_str}, ?)", [getattr(obj, f) for f in self.fields] + [None])
@@ -26,7 +27,7 @@ class SQLiteRepository:
         return obj
 
     def get(self, pk: int) -> T:
-        # Retrieve the object with the given primary key
+        # Получение объекта с заданным первичным ключом
         self.cursor.execute(f"SELECT {', '.join(self.fields)} FROM {self.table_name} WHERE pk = ?", (pk,))
         row = self.cursor.fetchone()
         if row is None:
@@ -38,13 +39,13 @@ class SQLiteRepository:
         return obj
 
     def update(self, obj: T) -> T:
-        # Update the object in the table
+        # Обновление объектов в таблице
         fields_str = ', '.join([f"{field}=?" for field in self.fields])
         self.cursor.execute(f"UPDATE {self.table_name} SET {fields_str} WHERE pk=?", [getattr(obj, f) for f in self.fields] + [getattr(obj, 'pk')])
         self.conn.commit()
         return obj
 
     def delete(self, pk: int) -> None:
-        # Delete the object with the given primary key
+        # Удаление объекта с заданным первичным ключом
         self.cursor.execute(f"DELETE FROM {self.table_name} WHERE pk=?", (pk,))
         self.conn.commit()
